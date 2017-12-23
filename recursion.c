5a50ec8b3ab75cf4551354f089f1cb018bd83f85
Unknown <mjhaveri@bu.edu>
2017-12-23 18:45:06 -0500

    finished problem 1

--- recursion.c
+++ recursion.c
@@ -1,13 +1,35 @@
 #include <stdio.h> 
-void towerOfHanoi(int n, char sourcePeg, char auxPeg, char toPeg){
-    if(n ==1){
-        printf("%c to %c", sourcePeg, toPeg); 
+void towerOfHanoi(int n, int *sourceArr, int *destArr, int *auxArr);
+void problem1(int n){
+    //recursive problem 1
+    int A[n]; 
+    int B[n]; 
+    int C[n]; 
+    for(int i = 0; i < n; i++){
+        A[i] = (n - i); 
     }
-    towerOfHanoi(n-1, sourcePeg ,toPeg, auxPeg);
-    printf("%c to %c", sourcePeg, auxPeg); 
-    towerOfHanoi(n-1, auxPeg, sourcePeg, toPeg); 
+    towerOfHanoi(n, A, B, C); 
+    for(int i = 0; i < n; i++){
+        printf("%i\n", B[i]); 
+    }
 }
-int main(void){
-    towerOfHanoi(1, 'A', 'B', 'C'); 
+
+void towerOfHanoi(int n, int *sourceArr, int *destArr, int *auxArr){
+    if(n <= 0){
+        int tmp = destArr[n];
+        destArr[n] = sourceArr[n];  
+        sourceArr[n] = tmp;  
+        return;
+    }
+    towerOfHanoi(n-1, sourceArr, auxArr, destArr); 
+    int tmp = destArr[n];
+    destArr[n] = sourceArr[n];  
+    sourceArr[n] = tmp;  
+    towerOfHanoi(n-1, auxArr, destArr, sourceArr); 
+
+}
+int main(){
+    problem1(4); 
+
     return 0; 
 }
