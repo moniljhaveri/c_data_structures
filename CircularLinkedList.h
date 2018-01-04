cd55c09af295e399db293dc30acecf3e54a727f3
Unknown <mjhaveri@bu.edu>
2018-01-03 20:42:38 -0500

    added count circular linked list function

--- CircularLinkedList.h
+++ CircularLinkedList.h
@@ -1,8 +1,19 @@
-#include "DoublyLinkedList" 
+#include "DoublyLinkedList.h" 
 
 struct Node* initCircularNode(int data){
     struct Node* head = createNode(data); 
     head->next = head; 
     head->prev = head; 
+    return head; 
+}
+
+int countCircularNode(struct Node** head){
+    struct Node* curr = *head; 
+    int count = 1;
+    while((curr->next)&&(curr != *head)){
+        count++; 
+        curr = curr->next; 
+    } 
+    return count; 
 
 }
