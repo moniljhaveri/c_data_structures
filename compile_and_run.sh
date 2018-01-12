be115d8779752b9ea01ba67bd4b642e385fdd6d5
Unknown <mjhaveri@bu.edu>
2018-01-12 12:52:43 -0500

    updated compile file

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,4 +1,5 @@
 gcc -Wall -ggdb -o recursion recursion.c  -lm
 gcc -Wall -ggdb -o linkedList linked_list.c  -lm
 gcc -Wall -ggdb -o unrolledList unrolled_test.c  -lm
-./unrolledList
+gcc -Wall -ggdb -o linkedListProblems linkedlistproblem.c  -lm
+./linkedListProblems
