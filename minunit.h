b700eb666f5789007b918c4f5a091542007fc7ed
Unknown <mjhaveri@bu.edu>
2018-01-31 00:32:21 +0530

    created arr assert unit test function

--- minunit.h
+++ minunit.h
@@ -13,4 +13,18 @@
         if (message)            \
             return message;     \
     } while (0)
+inline int mu_arr_assert(int numElem, int *expArr, int *actArr) __attribute__((always_inline)); 
+inline int mu_arr_assert(int numElem, int *expArr, int *actArr)
+{
+    for(int i = 0; i < numElem; i++)
+    {
+        if(*expArr != *actArr){
+            return 0; 
+        }
+        expArr++; 
+        actArr++; 
+    }
+    return 1; 
+
+}
 extern int tests_run;
