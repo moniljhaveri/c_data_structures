f4afacc17497ebddf4a03075b4adc0e16da104e1
Unknown <mjhaveri@bu.edu>
2018-02-18 18:14:10 -0500

    created createListOfSize function

--- CircularLinkedList.h
+++ CircularLinkedList.h
@@ -1,71 +1,79 @@
-#include "DoublyLinkedList.h" 
+#include "DoublyLinkedList.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-void insertEndCircularNode(int data, struct Node ** head); 
-struct Node* initCircularNode(int data){
-    struct Node* head = createNode(data); 
-    head->next = head; 
-    head->prev = head; 
-    return head; 
+void insertEndCircularNode(int data, struct Node **head);
+struct Node *initCircularNode(int data)
+{
+    struct Node *head = createNode(data);
+    head->next = head;
+    head->prev = head;
+    return head;
 }
 
-int countCircularNode(struct Node** head){
-    struct Node* curr = *head; 
+int countCircularNode(struct Node **head)
+{
+    struct Node *curr = *head;
     int count = 1;
-    while((curr->next)&&(curr->next != *head)){
-        count++; 
-        curr = curr->next; 
-    } 
-    return count; 
-}
-void insertBeginningCircularNode(int data, struct Node** head){
-    struct Node* curr = *head; 
-    struct Node* newNode = createNode(data); 
-    while((curr->next != *head)){
-        curr = curr->next; 
+    while ((curr->next) && (curr->next != *head))
+    {
+        count++;
+        curr = curr->next;
     }
-    curr->next = newNode; 
-    newNode->next = *head; 
-    newNode->prev = (*head)->prev; 
-    (*head)->prev = newNode; 
-    *head = newNode; 
+    return count;
 }
-
-void insertEndCircularNode(int data, struct Node ** head){
-    struct Node* newNode = createNode(data); 
-    struct Node* curr = *head;
-    while(curr->next != *head){
-        curr = curr->next; 
+void insertBeginningCircularNode(int data, struct Node **head)
+{
+    struct Node *curr = *head;
+    struct Node *newNode = createNode(data);
+    while ((curr->next != *head))
+    {
+        curr = curr->next;
     }
     curr->next = newNode;
-    newNode->prev = curr; 
-    newNode->next = *head; 
-    (*head)->prev = newNode; 
+    newNode->next = *head;
+    newNode->prev = (*head)->prev;
+    (*head)->prev = newNode;
+    *head = newNode;
 }
 
-void deleteNodeFirstCircularNode( struct Node** head){
-    struct Node* curr = *head; 
-    struct Node* nextNode = curr->next; 
-    nextNode->prev = curr->prev; 
-    (curr->prev)->next = nextNode; 
-    *head = nextNode; 
-    free(curr); 
-
+void insertEndCircularNode(int data, struct Node **head)
+{
+    struct Node *newNode = createNode(data);
+    struct Node *curr = *head;
+    while (curr->next != *head)
+    {
+        curr = curr->next;
+    }
+    curr->next = newNode;
+    newNode->prev = curr;
+    newNode->next = *head;
+    (*head)->prev = newNode;
 }
 
-void printCircular(struct Node** head){
-    struct Node* curr = *head; 
+void deleteNodeFirstCircularNode(struct Node **head)
+{
+    struct Node *curr = *head;
+    struct Node *nextNode = curr->next;
+    nextNode->prev = curr->prev;
+    (curr->prev)->next = nextNode;
+    *head = nextNode;
+    free(curr);
+}
 
-    //printf("%i test \n", (*head)->data); 
-    if(*head == NULL){
-        return; 
+void printCircular(struct Node **head)
+{
+    struct Node *curr = *head;
+
+    //printf("%i test \n", (*head)->data);
+    if (*head == NULL)
+    {
+        return;
     }
 
-    do{
-        printf("%i\n", curr->data); 
-        curr = curr->next; 
-    }
-    while(curr != *head); 
-
+    do
+    {
+        printf("%i\n", curr->data);
+        curr = curr->next;
+    } while (curr != *head);
 }
