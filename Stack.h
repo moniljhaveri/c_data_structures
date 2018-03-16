e913eb3578c96ca86a98cf879ec81875752f6e5f
Unknown <mjhaveri@bu.edu>
2018-03-15 22:11:10 -0400

    created stack

--- Stack.h
+++ Stack.h
@@ -1,5 +1,6 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+#include <stdint.h> 
 
 typedef struct Stack
 {
@@ -8,67 +9,79 @@ typedef struct Stack
     int *data
 } Stack; 
 
+unsigned int returnSize(Stack *stack)
+{
+    return (stack->size); 
+} 
+
+unsigned int returnIdx(Stack *stack)
+{
+    return (stack->idx); 
+}
+
+void reduce(Stack *stack)
+{
+
+    int *arr = (int*)malloc(sizeof(int) * (stack->size /4)); 
+    
+    for(int i = 0; i < (stack->size/4); i++)
+    {
+        arr[i] = stack->data[i]; 
+    }
+
+    free(stack->data); 
+    stack->data = arr; 
+    stack->size /= 4; 
+}
+
+void enlarge(Stack *stack)
+{
+    int *arr = (int*)malloc(sizeof(int) * stack->size * 2); 
+    
+    for(int i = 0; i < stack->size; i++)
+    {
+        arr[i] = stack->data[i]; 
+    }
+
+    free(stack->data); 
+    stack->data = arr; 
+    stack->size *= 2; 
+}
+
+
 Stack* createStack()
 {
-    Stack *stack = (stack*)malloc(sizeof(Stack)); 
-    stack.size = 2;  
-    stack.idx = 0; 
-    stack.data = (int*)malloc(sizeof(int) * stack.size); 
+    Stack *stack = (Stack*)malloc(sizeof(Stack)); 
+    stack->size = 2;  
+    stack->idx = 0; 
+    stack->data = (int*)malloc(sizeof(int) * stack->size); 
     return stack; 
 }
 
 void push(int data, Stack *stack)
 {
-    if((stack.idx + 1) == stack.size)
+    if((stack->idx + 1) == stack->size)
     {
         enlarge(stack); 
     }
-    stack.data[stack.idx] = data; 
-    stack.idx++; 
-}
-
-void enlarge(Stack *stack)
-{
-    int *arr = (int*)malloc(sizeof(int) * stack.size * 2); 
-    
-    for(int i = 0; i < stack.size; i++)
-    {
-        arr[i] = stack.data[i]; 
-    }
-
-    free(stack.data); 
-    stack.data = arr; 
-    stack.size *= 2; 
+    stack->data[stack->idx] = data; 
+    stack->idx++; 
 }
 
 int top(Stack *stack)
 {
-    return stack.data[stack.idx]; 
+    return stack->data[stack->idx]; 
 }
 
 void pop(Stack *stack)
 {
-    if(!stack.idx)
+    if(!stack->idx)
     {
         return; 
     }
-    if(stack.idx == stack.size/4)
+    if(stack->idx == stack->size/4)
     {
         reduce(stack); 
     }
-    stack.idx--; 
-}
-void reduce(Stack *stack)
-{
-
-    int *arr = (int*)malloc(sizeof(int) * (stack.size /4)); 
-    
-    for(int i = 0; i < (stack.size/4); i++)
-    {
-        arr[i] = stack.data[i]; 
-    }
-
-    free(stack.data); 
-    stack.data = arr; 
-    stack.size /= 4; 
+    stack->idx--; 
 }
