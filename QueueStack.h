dd8e7e52588e900130403a7fe02cff2772d875de
Unknown <mjhaveri@bu.edu>
2018-03-20 22:19:20 -0400

    created QueueStack file

---
+++ QueueStack.h
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <Stack.h> 
+
+typedef struct QueueStack 
+{
+    Stack *stack0; 
+    Stack *stack1; 
+} QueueStack; 
+
+QueueStack* createQueueStack()
+{
+    QueueStack *q = (QueueStack*)malloc(sizeof(QueueStack)); 
+    return q; 
+}
